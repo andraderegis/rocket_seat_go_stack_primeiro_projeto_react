f7f6c08406c0a7430baf89ecde7671b2d1a3500f
andraderegis <reginaldo.andrade@outlook.com>
2021-02-01 14:34:54 -0300

    Styling dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../asserts/logo.svg';
 
-const Dashboard: React.FC = () => <Title>Expore repositórios no Github</Title>;
+import { Form, Repositories, Title } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="test">
+        <img src="https://avatars.githubusercontent.com/u/25646286?s=460&u=226bb3bdb40561b90895364475609d438b5a2c93&v=4" alt="Regis Andrade" />
+        <div>
+          <strong>andraderegis/rocket_seat_go_stack_desafio_database_upload</strong>
+          <p>Atividade do desafio database upload</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="test">
+        <img src="https://avatars.githubusercontent.com/u/25646286?s=460&u=226bb3bdb40561b90895364475609d438b5a2c93&v=4" alt="Regis Andrade" />
+        <div>
+          <strong>andraderegis/rocket_seat_go_stack_desafio_database_upload</strong>
+          <p>Atividade do desafio database upload</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="test">
+        <img src="https://avatars.githubusercontent.com/u/25646286?s=460&u=226bb3bdb40561b90895364475609d438b5a2c93&v=4" alt="Regis Andrade" />
+        <div>
+          <strong>andraderegis/rocket_seat_go_stack_desafio_database_upload</strong>
+          <p>Atividade do desafio database upload</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
