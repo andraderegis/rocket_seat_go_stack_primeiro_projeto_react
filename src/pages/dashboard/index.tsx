fea6f5d3d5dbd4a745f9403d4262964cbcba33b6
andraderegis <reginaldo.andrade@outlook.com>
2021-02-01 11:16:54 -0300

    Add first routes and initialize stylization with styled-components

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => <Title>Expore repositórios no Github</Title>;
+
+export default Dashboard;
---
+++ src/pages/dashboard/styles.ts
@@ -0,0 +1,6 @@
+import styles from 'styled-components';
+
+export const Title = styles.h1`
+  font-size: 48px;
+  color: #3A3A3A
+`;
